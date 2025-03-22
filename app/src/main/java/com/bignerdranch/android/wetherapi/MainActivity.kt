0f50e1bf258e834796cb398f27ed028c652898ef
tematrkv <tatarnikov.stud@mail.ru>
2025-03-22 22:51:39 +0700

    Создание главного фрагмента MainFragment для отображения основной информации и других фрагментов. Добавление MainFragment на MainActivity.kt

--- app/src/main/java/com/bignerdranch/android/wetherapi/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/wetherapi/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.bignerdranch.android.wetherapi
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.bignerdranch.android.wetherapi.Fragments.MainFragment
+import com.bignerdranch.android.wetherapi.databinding.ActivityMainBinding
+import org.json.JSONObject
 
+const val API_KEY = "705bc494d10c4699911154110252203"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.placeHolder, MainFragment.newInstance())
+            .commit()
     }
 }
