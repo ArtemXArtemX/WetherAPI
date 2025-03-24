fa7a13ae845200ec785143a1f1fe7f549ec33b55
tematrkv <tatarnikov.stud@mail.ru>
2025-03-24 16:44:19 +0700

    Проверка на то, дал ли пользователь разрешение на определение своего местоположения.

--- app/src/main/java/com/bignerdranch/android/wetherapi/Fragments/MainFragment.kt
+++ app/src/main/java/com/bignerdranch/android/wetherapi/Fragments/MainFragment.kt
@@ -8,8 +8,13 @@ import android.view.ViewGroup
 import com.bignerdranch.android.wetherapi.R
 import com.bignerdranch.android.wetherapi.databinding.ActivityMainBinding
 import com.bignerdranch.android.wetherapi.databinding.FragmentMainBinding
+import android.Manifest
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainFragment: Fragment(){
+    private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +22,24 @@ class MainFragment: Fragment(){
     ): View? {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    private fun permissionListener(){
+        pLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private  fun checkPermission(){
+        if(!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     companion object {
