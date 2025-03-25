170fd374ed1b692591628d0769fcecd919c0010f
tematrkv <tatarnikov.stud@mail.ru>
2025-03-25 17:30:06 +0700

    Создан DataClass WeatherModel, изменен тип MutableLiveData на WeatherModel в MainViewModel, добавлена разметка элемента списка list_item.xml.

--- app/src/main/java/com/bignerdranch/android/wetherapi/MainViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/wetherapi/MainViewModel.kt
@@ -2,8 +2,9 @@ package com.bignerdranch.android.wetherapi
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.bignerdranch.android.wetherapi.adapters.WeatherModel
 
 class MainViewModel : ViewModel() {
-    // val liveDataCurrent: MutableLiveData<String>()
-    //  val liveDataList: MutableLiveData<List<String>>()
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
+    val liveDataList = MutableLiveData<List<WeatherModel>>()
 }
