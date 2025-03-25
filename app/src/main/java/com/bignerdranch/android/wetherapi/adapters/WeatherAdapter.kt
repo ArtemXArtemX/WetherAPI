455d8924ebb442609c589ea11fa61da0e0ea1e6b
tematrkv <tatarnikov.stud@mail.ru>
2025-03-25 17:45:00 +0700

    Добавлен RecyclerView в fragment_hours.xml, создан RecyclerViewAdapter, добавлен RecyclerViewHolder.

---
+++ app/src/main/java/com/bignerdranch/android/wetherapi/adapters/WeatherAdapter.kt
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.wetherapi.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bignerdranch.android.wetherapi.databinding.ListItemBinding
+
+class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
+    class Holder(view: View) : RecyclerView.ViewHolder(view){
+        val binding = ListItemBinding.bind(view)
+        fun bind(item: WeatherModel) = with(binding){
+            tvDate.text = item.time
+            tvCondition.text = item.condition
+            tvTemp.text = item.currentTemp
+        }
+    }
+}
