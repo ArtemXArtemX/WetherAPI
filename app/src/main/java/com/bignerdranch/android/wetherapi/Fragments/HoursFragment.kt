dd3a79d171cbd0e69914d8300a27cac7c8cdefce
tematrkv <tatarnikov.stud@mail.ru>
2025-03-25 18:03:53 +0700

    Создан Comparator (DiffUtil.ItemCallback) в WeatherAdapter, добавлены onCreateViewHolder и onBindViewHolder в WeatherAdapter, инициализирован RecyclerView в HoursFragment.

--- app/src/main/java/com/bignerdranch/android/wetherapi/Fragments/HoursFragment.kt
+++ app/src/main/java/com/bignerdranch/android/wetherapi/Fragments/HoursFragment.kt
@@ -6,15 +6,55 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bignerdranch.android.wetherapi.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bignerdranch.android.wetherapi.adapters.WeatherAdapter
+import com.bignerdranch.android.wetherapi.adapters.WeatherModel
+import com.bignerdranch.android.wetherapi.databinding.FragmentHoursBinding
+import com.bignerdranch.android.wetherapi.databinding.FragmentMainBinding
 
 
 class HoursFragment : Fragment() {
+    private lateinit var binding: FragmentHoursBinding
+    private lateinit var adapter: WeatherAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hours, container, false)
+        binding = FragmentHoursBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRcView()
+    }
+
+    private fun initRcView() = with(binding){
+        rcView.layoutManager = LinearLayoutManager(activity)
+        adapter = WeatherAdapter()
+        rcView.adapter = adapter
+        val list = listOf(
+            WeatherModel(
+                "",
+                "12:00",
+                "Sunny",
+                "25°C", "", "", "", ""
+            ),
+            WeatherModel(
+                "",
+                "13:00",
+                "Sunny",
+                "27°C",
+                "", "", "", ""
+            ),
+            WeatherModel(
+                "",
+                "14:00",
+                "Sunny",
+                "35°C",
+                "", "", "", "")
+        )
+        adapter.submitList(list)
     }
 
     companion object {
